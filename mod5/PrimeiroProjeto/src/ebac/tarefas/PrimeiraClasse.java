0bd512380bf309b439778f7f5512bc247915a08c
Yuji <g.yujinak@gmail.com>
2023-11-03 17:05:47 -0300

    first commit, adicionando tarefa módulo 5

---
+++ mod5/PrimeiroProjeto/src/ebac/tarefas/PrimeiraClasse.java
@@ -0,0 +1,10 @@
+package ebac.tarefas;
+
+public class PrimeiraClasse {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello Yuji");
+	}
+
+}
