8b222bd9c4ddce8121e0ae4c5c19fbdb8a4aeea5
Yuji <g.yujinak@gmail.com>
2023-11-06 17:25:55 -0300

    adicionando exercicio módulo 8

---
+++ mod8/Notas/src/sistema/notas/Main.java
@@ -0,0 +1,16 @@
+package sistema.notas;
+
+public class Main {
+    public static void main(String[] args) {
+
+            float nota1 = 5.4f;
+            float nota2 = 8.2f;
+            float nota3 = 6.4f;
+            float nota4 = 7.7f;
+
+            float mediaFinal = (nota1 + nota2 + nota3 + nota4) / 4;
+
+            System.out.println(mediaFinal);
+
+    }
+}
