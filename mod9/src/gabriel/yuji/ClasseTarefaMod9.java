e73d734e0f0cf381c1281e59d028f7ca1440507f
Yuji <g.yujinak@gmail.com>
2023-11-14 19:08:24 -0300

    adicionando alterações mod8 e first commit de mod9

---
+++ mod9/src/gabriel/yuji/ClasseTarefaMod9.java
@@ -0,0 +1,20 @@
+package gabriel.yuji;
+
+public class ClasseTarefaMod9 {
+
+	public static void main(String[] args) {
+		// Primitive
+		int n1 = 10;
+		System.out.println(n1);
+		
+		// Casting
+		short n2 = (short) n1;
+		System.out.println(n2);
+		
+		// Boxing (Wrapper)
+		Integer n3 = 100;
+		System.out.println(n3);
+
+	}
+
+}
